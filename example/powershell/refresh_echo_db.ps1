6cbf106428b9233e7fe85936eb8267f9050edad5
Moti Nanikashvili <moti.nanikashvili@silk.us>
2025-12-14 14:17:28 +0000

    Add EULA to powershell script

--- example/powershell/refresh_echo_db.ps1
+++ example/powershell/refresh_echo_db.ps1
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 Silk Technologies, Inc.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 <#
 .SYNOPSIS
     Refreshes a Silk Echo database from a new snapshot of its source.
