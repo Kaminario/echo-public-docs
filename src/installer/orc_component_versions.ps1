6ba011fcfa784e5d6065c55efb7c13662aa3d940
Ilya Levin <ilya.levin@silk.us>
2026-02-05 12:01:19 +0200

    Lock silk-agent and svss versions
    Move installer URLs to separate file with
    explicit version numbers instead of using
    floating "latest" references.

---
+++ src/installer/orc_component_versions.ps1
@@ -0,0 +1,7 @@
+# Component versions - update these to lock specific releases
+Set-Variable -Name SilkAgentVersion -Value "v1.1.8-20260129-123104" -Option AllScope -Scope Script
+Set-Variable -Name SvssVersion -Value "2.0.18" -Option AllScope -Scope Script
+
+# Installer URLs
+Set-Variable -Name SilkAgentURL -Value "https://storage.googleapis.com/silk-public-files/silk-agent-installer-$SilkAgentVersion.exe" -Option AllScope -Scope Script
+Set-Variable -Name SilkVSSURL -Value "https://storage.googleapis.com/silk-public-files/svss-$SvssVersion.exe" -Option AllScope -Scope Script
